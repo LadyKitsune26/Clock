099ceb34d060bfe5a6d835e695f99552b7560bd4
LadyKitsune26 <langlois.raimey@gmail.com>
2025-11-12 20:50:53 -0600

    clock done

--- Clock Solution/app.js
+++ Clock Solution/app.js
@@ -1,13 +1,25 @@
-const secondHandle = document.querySelector('.handle__second');
-const minuteHandle = document.querySelector('.handle__minute');
-const hourHandle = document.querySelector('.handle__hour'); 
+const secondHandle = document.querySelector(".handle__second");
+const minuteHandle = document.querySelector(".handle__minute");
+const hourHandle = document.querySelector(".handle__hour");
 
+setInterval(() => {
+  setHandles();
+}, 1000);
 
-const d = new Date()
-const hours = d.getHours()
-const minutes = d.getMinutes()
-const seconds = d.getSeconds()
+function setHandles() {
+  const d = new Date();
 
-console.log(hourHandle)
+  const hours = d.getHours();
+  const minutes = d.getMinutes();
+  const seconds = d.getSeconds();
 
-hourHandle.style.transform = `translateX(-50%) rotate(${hours * 30})`
+  const extraMinutesAngle = seconds * 0.1;
+  const minutesAngle = minutes * 6 + extraMinutesAngle;
+
+  const extraHoursAngle = minutes * 0.5;
+  const hoursAngle = hours * 30 + extraHoursAngle;
+
+  hourHandle.style.transform = `translateX(-50%) rotate(${hoursAngle}deg)`;
+  minuteHandle.style.transform = `translateX(-50%) rotate(${minutesAngle}deg)`;
+  secondHandle.style.transform = `translateX(-50%) rotate(${seconds * 6}deg)`;
+}
